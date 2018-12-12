2a474811eb53ca9bdbf63e91e05b830d2c03025e
cuijj <jiajie.cui@midea.com.cn>
2018-12-12 15:17:37 +0800

    加入静态文件

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const static = require('koa-static')
 const jsonp = require('koa-jsonp')
+const path = require('path')
 
 const app = new Koa()
 let api = new Router()
 
+const staticPath = './static'
+
 app.use(jsonp())
+app.use(static(
+    path.join(__dirname, staticPath)
+))
 
 api.get('/', async(ctx) => {
     ctx.body = '崔叔的个人网站'
